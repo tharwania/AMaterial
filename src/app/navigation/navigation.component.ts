525f40618b0d9323193b17d8a6c214475662ffad
tharwania <tharwania@gmail.com>
2019-07-25 18:01:02 +0400

    Navigation Icon Meanu

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -9,7 +9,46 @@ import { map, share } from 'rxjs/operators';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
-
+  folders = [
+    {
+      name: 'Updates',
+      icon: 'notification_important'
+    },
+    {
+      name: 'Messages',
+      icon: 'message'
+    },
+    {
+      name: 'Comments',
+      icon: 'comment'
+    },
+    {
+      name: 'Tasks',
+      icon: 'format_list_numbered'
+    }
+  ];
+  notes = [
+    {
+      name: 'Profile',
+      icon: 'account_circle'
+    },
+    {
+      name: 'Payments',
+      icon: 'payment'
+    },
+    {
+      name: 'Projects',
+      icon: 'account_balance'
+    },
+    {
+      name: 'Lock Account',
+      icon: 'lock'
+    },
+    {
+      name: 'Logout',
+      icon: 'exit_to_app'
+    }
+  ];
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
